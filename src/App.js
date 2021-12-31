9f043c9a34d419ec7b22425e5ddf601874f592e2
mntak <32028272+mntak@users.noreply.github.com>
2021-12-31 10:07:43 -1000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by {""}
-        <a 
-        href="https://app.netlify.com/sites/friendly-mayer-5513d1/overview"
-        target="_blank">
-          Mihae Tak </a>and is{" "} 
-        <a
-        href="https://github.com/mntak/react-weather-app"
-        target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
-       </div>
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://app.netlify.com/sites/friendly-mayer-5513d1/overview"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mihae Tak{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/mntak/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
   );
 }
 
