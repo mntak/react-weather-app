c650c041bca27c3b160b0b02a99e7e457b37fb3a
mntak <32028272+mntak@users.noreply.github.com>
2021-12-31 09:53:34 -1000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
      <div className="App">
-Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a 
+        href="https://app.netlify.com/sites/friendly-mayer-5513d1/overview"
+        target="_blank">
+          Mihae Tak </a>and is{" "} 
+        <a
+        href="https://github.com/mntak/react-weather-app"
+        target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
        </div>
   );
 }
