207e0ad567887eeee53243918f95ac8f2dfd05be
mntak <32028272+mntak@users.noreply.github.com>
2021-12-31 20:37:48 -1000

    Added weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
     });
   }
 
